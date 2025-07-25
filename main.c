7f792607d72f5c25e9d3ea770f2bc8e2ff584afc
rd70 <rd70@boomtech.cn>
2025-07-25 18:02:22 +0800

    add print

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 int main(int argc, char const *argv[])
 {
-    /* code */
+    print();
     return 0;
 }
---
+++ print.c
@@ -0,0 +1,6 @@
+int print(int argc, char const *argv[])
+{
+    /* code */
+    printf("hello world\n");
+    return 0;
+}
